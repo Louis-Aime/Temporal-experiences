5700041c412cb9cf757f10561649f9d9d2d942f5
Louis-Aimé de Fouquières <louis-aime@calendriermilesien.org>
2020-11-13 16:20:42 +0100

    Embedd in Calendars.js

--- TemporalEnvironment.js
+++
@@ -1,7 +0,0 @@
-/* Date environment: if legacy Date, monthbase is 0. If Temporal, monthbase is 1. Define also isoJD.
-*/
-/*const dateEnvironment = {
-	monthBase : 1,
-	julianDayIso1 : new JulianDayIso(1)	// the parameter is the number of the first month. // ex isoJD
-}*/
-const JDConvert = new JulianDayIso(1);
